8a1a76bfedec5c4d43cd95889c8517442377fc3f
ian.moss <ianmsieg@gmail.com>
2023-01-22 16:03:25 -0600

    added notification functionality to profiles

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -6,14 +6,33 @@ export default class extends Controller {
     toggleForm(e) {
         e.preventDefault()
         e.stopPropagation()
-        console.log(e.params.form)
         const formId = e.params.form
         const commentBodyId = e.params.body
+        const editButtonId = e.params.edit
         
         const form = document.getElementById(formId)
+        const commentBody = document.getElementById(commentBodyId)
+        const editButton = document.getElementById(editButtonId)
+        
         form.classList.toggle('d-none')
         form.classList.toggle('mt-3')
-        const commentBody = document.getElementById(commentBodyId)
         commentBody.classList.toggle('d-none')
+
+        this.toggleEditButton(editButton)
+    }
+    
+    toggleEditButton(editButton) {
+        if (editButton.innerText === 'Edit') {
+            editButton.innerText = 'Cancel'
+            this.toggleEditButtonClass(editButton)
+        } else {
+            editButton.innerText = 'Edit'
+            this.toggleEditButtonClass(editButton)
+        }
+    }
+
+    toggleEditButtonClass(editButton) {
+        editButton.classList.toggle('btn-secondary')
+        editButton.classList.toggle('btn-warning')
     }
 }
