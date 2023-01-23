d536b585de2b30b99af03f45ba0bb42d31846a11
ian.moss <ianmsieg@gmail.com>
2023-01-22 21:23:43 -0600

    added admin profile and admin control page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'admin@test.com',
+    password: 'password',
+    password_confirmation: 'password',
+    name: 'Admin',
+    role: User.roles[:admin]
+)
 
-User.create(email: 'test@test.com', name: 'test', password: 'password', password_confirmation: 'password')
-User.create(email: 'john@test.com', name: 'john', password: 'password', password_confirmation: 'password')
+User.create(email: 'test@test.com',
+    password: 'password',
+    password_confirmation: 'password',
+    name: 'Test'
+)
 
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x} Words go here idk", user_id: User.first.id )
+    post = Post.create(title: "Title #{x}", body: "Body #{x} Words go here idk", user_id: User.first.id )
+
+    5.times do |y|
+        Comment.create(body: "Comment #{y}",
+            user_id: User.second.id,
+            post_id: post.id 
+        )
+    end
 end
